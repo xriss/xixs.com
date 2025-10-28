01f73661ac6077b652310118f773b0641997a32f
kriss <kriss@xixs.com>
2025-10-28 02:44:31 +0000

    .

---
+++ build.sh
@@ -0,0 +1,10 @@
+cd `dirname $0`
+
+rsync -avh ../../gamecake/exe     plated/source/wasm/ --delete --human-readable
+rsync -avh ../../gamecake/html/js plated/source/wasm/ --delete --human-readable
+
+rm plated/source/wasm/exe/.git
+
+plated/build
+
+
